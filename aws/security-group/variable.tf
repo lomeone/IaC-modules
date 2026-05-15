ed52a4990a7ace5702e2d86d7623e405cfe11372
comstering <comstering@gmail.com>
2026-05-16 00:47:56 +0900

    refactor: Refactor aws security group module

--- aws/security-group/variable.tf
+++ aws/security-group/variable.tf
@@ -19,19 +19,21 @@ variable "security_group_info" {
       protocol          = string,
       from_port         = number,
       to_port           = number,
-      cidr_block_v4     = optional(string, null),
-      cidr_block_v6     = optional(string, null),
-      security_group_id = optional(string, null)
-      prefix_list_id    = optional(string, null)
-    })),
-    egress = list(object({
-      protocol          = optional(string, "-1"),
-      from_port         = optional(number, "-1"),
-      to_port           = optional(number, "-1"),
       cidr_block_v4     = optional(string, null)
       cidr_block_v6     = optional(string, null)
       security_group_id = optional(string, null)
       prefix_list_id    = optional(string, null)
+      allow_self        = optional(bool, false)
+    })),
+    egress = list(object({
+      protocol          = optional(string, "-1")
+      from_port         = optional(number, -1)
+      to_port           = optional(number, -1)
+      cidr_block_v4     = optional(string, null)
+      cidr_block_v6     = optional(string, null)
+      security_group_id = optional(string, null)
+      prefix_list_id    = optional(string, null)
+      allow_self        = optional(bool, false)
     }))
   })
 }
