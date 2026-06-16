fe8b550220acdcec39d2f329f3854594ad07b97e
comstering <comstering@gmail.com>
2026-06-16 23:51:28 +0900

    feat: Add cloudflare provider

--- cloudflare/dns/records/main.tf
+++ cloudflare/dns/records/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
+  }
+}
+
 locals {
   proxiable_types = toset(["A", "AAAA", "CNAME"])
 
